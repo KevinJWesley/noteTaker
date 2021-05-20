40197e6e1a05d50cb643bb29d192074100de0a02
kevinjwesley <kevin.john.wesley@gmail.com>
2021-05-19 19:24:15 -0600

    working on API Routes in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes
+// HTML Routes
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
 );
@@ -15,5 +15,19 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
+// API Routes
+app.get("/api/notes", (req, res) => res.json(note));
+
+// create notes with JSON input
+app.post("/api/notes", (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newNotes = req.body;
+
+  console.log(newNotes);
+
+  notes.push(newNotes);
+  res.json(newNotes);
+});
 // kicks off server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
