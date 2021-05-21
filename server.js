012926d166c5a180197f0f4fccdc59f122498026
kevinjwesley <kevin.john.wesley@gmail.com>
2021-05-20 20:47:44 -0600

    added delete function

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8080;
@@ -18,18 +19,67 @@ app.get("/notes", (req, res) =>
 );
 
 // API Routes
-app.get("/api/notes", (req, res) => res.json(note));
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// GET NOTES
 
-// create notes with JSON input
+app.get("/api/notes", (req, res) => {
+  let notes = fs.readFileSync("./db/db.json", "utf8");
+  notes = JSON.parse(notes);
+  // console.log(notes);
+
+  res.json(notes);
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+// unique id
+const newUuid = () => {
+  let letters = "6b13e2d0f4c5a".split("");
+  let indexLengthArr = [8, 4, 4, 4, 12];
+  let res = ["", "", "", "", ""];
+  for (let i = 0; i < indexLengthArr.length; i++) {
+    let indexLength = indexLengthArr[i];
+    for (let j = 0; j < indexLength; j++) {
+      let randomIndex = Math.floor(Math.random() * indexLength);
+      res[i] += letters[randomIndex];
+    }
+  }
+  return res.join("-");
+};
+
 app.post("/api/notes", (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
+
   const newNotes = req.body;
-
-  console.log(newNotes);
-
+  let notes = fs.readFileSync("./db/db.json", "utf8");
+  notes = JSON.parse(notes);
+  req.body.id = newUuid();
   notes.push(newNotes);
-  res.json(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+
+  // console.log(newNotes);
+  notes = JSON.parse(notes);
+  // db.json.push(newNotes);
+  res.json(notes);
 });
+
+app.delete("/api/notes/:id", (req, res) => {
+  let notes = fs.readFileSync("./db/db.json", "utf8");
+  notes = JSON.parse(notes);
+
+  let browserId = req.params.id;
+
+  notes = notes.filter((note) => {
+    return note.id != browserId;
+  });
+  // console.log(notes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+
+  // console.log(newNotes);
+  notes = JSON.parse(notes);
+  // db.json.push(newNotes);
+  res.json(notes);
+});
+
 // kicks off server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
