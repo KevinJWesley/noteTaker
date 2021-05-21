e0d3ac98a60e4e0543ab73f371864c48bfd90214
kevinjwesley <kevin.john.wesley@gmail.com>
2021-05-21 16:03:56 -0600

    ss

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
 
 // unique id
 const newUuid = () => {
@@ -46,6 +46,8 @@ const newUuid = () => {
   }
   return res.join("-");
 };
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post("/api/notes", (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
